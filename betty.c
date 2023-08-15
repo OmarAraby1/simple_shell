fc8bb445044000ecafd9197d5a84cce0d19dfebf
Omar Araby <omararaby2001@gmail.com>
2023-08-15 14:37:34 +0300

    Beety Would Be Proud

---
+++ betty.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - Betty WOuld BE Proud
+ * Return: 0 Success
+ */
+
+int main()
+{
+	printf("Betty Would Be PRoud");
+	return (0);
+}
