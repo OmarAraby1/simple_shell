d74467cfbf56b3cbdfbb95e8a6a4431f0f2980c0
Omar Araby <72764839+OmarAraby1@users.noreply.github.com>
2023-08-18 17:03:01 +0300

    Our Own getenv

--- _getenv.c
+++ _getenv.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * _getenv - get environment from the PATH
+ * @var: The name of the environment
+ * Return: pointer to the environment
+ */
+
+char *_getenv(const char *var)
+{
+  int i, len;
+  
+  len = _strlen(var);
+	for (i = 0; environ[i]; i++)
+  {
+    if (_strncmp(var, environ[i], len) == 0)
+    {
+      return (environ[i]);
+    }
+  }
+  return (NULL);
+}
