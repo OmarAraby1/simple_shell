780dc9b09cda891ecb7ecc380052697c12d5a274
Omar Araby <omararaby2001@gmail.com>
2023-08-19 02:31:31 -0700

    BOSS HEAD

--- main.h
+++ main.h
@@ -9,17 +9,26 @@
 #include <math.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <dirent.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+extern char **environ;
 
 int main(int argc, char **argv);
 void _execute(char **argv);
 char *_pathFinder(char *exe);
-char *_getenv(const char *var);
+char *_getenv(char *var);
+void _print_env(void);
 
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 int _putchar(char c);
 
 #endif
