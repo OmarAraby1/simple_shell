1ef49e784a842ddfbf2ca82a307cbc7f155a3cee
Omar Araby <omararaby2001@gmail.com>
2023-08-19 03:03:19 -0700

    print environment variable

--- _print_env.c
+++ _print_env.c
@@ -8,10 +8,12 @@
 void _print_env(void)
 {
 	char **env = environ;
+	char nl = '\n';
 
 	while (*env != NULL)
 	{
-		printf("%s\n", *env);
+		write(STDOUT_FILENO, *env, _strlen(*env));
+		write(STDOUT_FILENO, &nl, 1);
 		env++;
 	}
 }
