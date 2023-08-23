4b7419d3ddbcb8d3718b6aa26770e46d00b0a9d6
Omar Araby <omararaby2001@gmail.com>
2023-08-23 10:57:24 -0700

    Free Linked Lists & Arguments

--- _free.c
+++ _free.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
 /**
- * _free - frees a string of strings
- * @pp: pointer to pointer
- * Return: NOTHING
+ * free_args - Frees up memory taken by args
+ * @args: list of input arguments
+ * @front: A double pointer to the beginning of args
+ */
+void free_args(char **args, char **front)
+{
+	size_t i;
+
+	for (i = 0; args[i] || args[i + 1]; i++)
+		free(args[i]);
+	free(front);
+}
+
+/**
+ * free_list - Frees a list_t linked list
+ * @head: The head of the list_t list
  */
 
-void _free(char **pp)
+void free_list(list_t *head)
 {
-	char **a = pp;
+	list_t *next;
 
-	if (!pp)
-		return;
-	while (*pp)
-		free(*pp++);
-	free(a);
+	while (head)
+	{
+		next = head->next;
+		free(head->dir);
+		free(head);
+		head = next;
+	}
 }
