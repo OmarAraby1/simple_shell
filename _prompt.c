0aee357905079f8c567ab3c242b26921f4e8b080
Omar Araby <omararaby2001@gmail.com>
2023-08-23 03:50:23 -0700

    Main Prompt

--- _prompt.c
+++ _prompt.c
@@ -1,99 +1,50 @@
 #include "main.h"
+
 /**
- * _tokenize - turn a string to tokens
- * @lineptr: the string input
- * @lineptr2: copy of lineptr
- * @dilem: the delimeter
- * Return: list of tokens
+ * sighand - action for SIGINT change
+ * @sig: value for SIGINT
  */
 
-char **_tokenize(char *lineptr, char *lineptr2, const char *dilem)
+void sighand(int sig)
 {
-	char **argv, *tok;
-	int n_tok = 0, i;
-
-	tok = strtok(lineptr, dilem);
-	while (tok != NULL)
-	{
-		n_tok++;
-		tok = strtok(NULL, dilem);
-	}
-	n_tok++;
-	argv = malloc(sizeof(char *) * n_tok);
-	tok = strtok(lineptr2, dilem);
-	for (i = 0; tok != NULL; i++)
-	{
-		argv[i] = malloc(sizeof(char) * _strlen(tok));
-		if (argv == NULL)
-		{
-			free(lineptr2);
-			free(argv);
-			perror("memory allocation error");
-			return (NULL);
-		}
-		_strcpy(argv[i], tok);
-		tok = strtok(NULL, dilem);
-	}
-	argv[i] = NULL;
-	free(lineptr2);
-	return (argv);
+	if (sig == SIGINT)
+	write(STDOUT_FILENO, "\n($) ", 5);
 }
 
 /**
- * main - My Propmt
- * @argc: number of passed arguments
- * @argv: list of passed arguments
+ * main - my prompt main
+ *@argc: arg count
+ *@argv: list of parameters
  * Return: 0 Success
  */
-
 int main(int argc, char **argv)
 {
-	ssize_t out_gl;
-	char *pr = "oi$ ", *lineptr, *lineptr2, *name;
-	const char *dilem = " \n";
-	size_t n = 0;
-	int check;
-	pid_t pid;
-	(void)argc;
+size_t buff_size = 0;
+ssize_t out_get = 1;
+char *buff;
+int nwords = 0;
 
-	name = argv[0];
-	while (1)
+(void)argc, (void)argv;
+
+	while (out_get > 0)
 	{
+		signal(SIGINT, sighand);
 		if (isatty(STDIN_FILENO))
-			write(STDIN_FILENO, pr, 4);
-		out_gl = getline(&lineptr, &n, stdin);
-		if (out_gl == -1)
-			break;
-		if (_strcmp(lineptr, "exit\n") == 0)
-			break;
-		if (_strcmp(lineptr, "env\n") == 0)
-		{_print_env();
-			continue;
-		}
-		lineptr2 = malloc(sizeof(char) * out_gl);
-		if (lineptr2 == NULL)
-		{perror("memory allocation error");
-			return (-1);
-		}
-		_strcpy(lineptr2, lineptr);
-		argv = _tokenize(lineptr, lineptr2, dilem);
-		check = _check(argv, name);
-		if (check == 0)
+			write(STDOUT_FILENO, "oi$ ", 4);
+		out_get = getline(&buff, &buff_size, stdin);
+		nwords = _count(buff);
+		if (out_get < 0)
 		{
-			{pid = fork();
-				if (pid == -1)
-				{perror("Error child:");
-					exit(1);
-				}
-				if (pid == 0)
-				{_execute(argv);
-					exit(0);
-				}
-				else
-					wait(NULL);
-			}
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(buff);
+			exit(EXIT_SUCCESS);
+		}
+		if (nwords > 0)
+		{
+			_check(buff, environ);
 		}
 	}
-	free(lineptr);
+	free(buff);
 	return (0);
 }
