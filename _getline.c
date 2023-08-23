e17a68dd1b5bee92b0e44be6a73067aee9d52902
Omar Araby <omararaby2001@gmail.com>
2023-08-23 10:58:03 -0700

    mimic getline()

--- _getline.c
+++ _getline.c
@@ -1,13 +1,56 @@
 #include "main.h"
 
 /**
- * assign_lineptr - Reassigns the lineptr variable for _getline.
- * @lineptr: A buffer to store an input string.
- * @n: The size of lineptr.
- * @buffer: The string to assign to lineptr.
- * @b: The size of buffer.
+ * _realloc - Reallocates a memory block
+ * @ptr: A pointer to memory
+ * @s_o: old size
+ * @s_n: new size
+ * Return: ptr or pointer to reallocated ot 0 or NULL
  */
-void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
+
+void *_realloc(void *ptr, unsigned int s_o, unsigned int s_n)
+{
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int i;
+
+	if (s_n == s_o)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		mem = malloc(s_n);
+		if (mem == NULL)
+			return (NULL);
+		return (mem);
+	}
+	if (s_n == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * s_n);
+	if (mem == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	filler = mem;
+	for (i = 0; i < s_o && i < s_n; i++)
+		filler[i] = *ptr_copy++;
+	free(ptr);
+	return (mem);
+}
+
+/**
+ * assign_lineptr - Reassigns the lineptr variable for _getline.
+ * @lineptr: A buffer to store an input string
+ * @n: The size of lineptr
+ * @buff: The string to assign to lineptr
+ * @b: The size of buffer
+ */
+
+void assign_lineptr(char **lineptr, size_t *n, char *buff, size_t b)
 {
 	if (*lineptr == NULL)
 	{
@@ -15,7 +58,7 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 			*n = b;
 		else
 			*n = 120;
-		*lineptr = buffer;
+		*lineptr = buff;
 	}
 	else if (*n < b)
 	{
@@ -23,20 +66,20 @@ void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b)
 			*n = b;
 		else
 			*n = 120;
-		*lineptr = buffer;
+		*lineptr = buff;
 	}
 	else
 	{
-		_strcpy(*lineptr, buffer);
-		free(buffer);
+		_strcpy(*lineptr, buff);
+		free(buff);
 	}
 }
 
 /**
- * _getline - Reads input from a stream.
+ * _getline - Reads input from a stream
  * @lineptr: A buffer to store the input
  * @n: The size of lineptr
- * @stream: The stream to read from.
+ * @stream: The stream to read from
  * Return: The number of bytes read
  */
 
